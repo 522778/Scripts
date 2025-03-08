cf7f9a71a2073b0cebc4021f28a0f7a8fb662ec9
Dmitrii Alekseev <d.alekseev@prequelapp.com>
2025-03-08 16:26:23 +0100

    add check_steam_wishlist.sh

---
+++ check_steam_wishlist.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# returns list of games with discount equal or more
+# see 'expected params'
+# profile wishlist should be public
+
+# check script variables
+
+if [ -n "$1" ]; then
+	PROFILE_ID=$1
+	DISCOUNT_EXPECTED=$2
+else
+	echo "Expected params:"
+	echo "1. Profile 64 id, ex. 76561198024952213"
+	echo "2. Expected discount, from 0 to 100"
+	exit 1
+fi
+
+# functions
+
+getWishlistAppIds() {
+	local API="https://api.steampowered.com/IWishlistService/GetWishlist/v1/?steamid="
+	local REQUEST="${API}${PROFILE_ID}"
+	local RESULT=$(curl -sl ${REQUEST} | grep -oP '"appid":\s*\K\d+' | paste -sd,)
+	echo "$RESULT"
+}
+
+getDiscountAppIds() {
+	local APP_ID_LIST=$1
+	local API="https://store.steampowered.com/api/appdetails?appids="
+	local REQUEST="${API}${APP_ID_LIST}&filters=price_overview"
+	local RESULT=$(curl -sl "${REQUEST}" | tr '{},' '\n' | grep -P '(\"\d+\"|discount_percent)' | sed 's/\"\|://g' | sed ':a;N;$!ba;s/\ndiscount_percent/_/g')
+	echo "$RESULT"
+}
+
+getAppUrl() {
+	local APP_ID=$1
+	echo "https://store.steampowered.com/app/${APP_ID}/"
+}
+
+# main
+
+WISHLIST_APP_IDS=$(getWishlistAppIds)
+DISCOUNT_APP_IDS=$(getDiscountAppIds "$WISHLIST_APP_IDS")
+
+for APP_ID_DISCOUNT in $DISCOUNT_APP_IDS; do
+	if [[ "$APP_ID_DISCOUNT" =~ "_" ]]; then
+		APP_ID="${APP_ID_DISCOUNT%%_*}"
+		DISCOUNT="${APP_ID_DISCOUNT##*_}"
+		if [[ "$DISCOUNT" -ge "$DISCOUNT_EXPECTED" ]]; then
+			APP_URL=$(getAppUrl "$APP_ID")
+			echo "$APP_URL"
+		fi
+	fi
+done
